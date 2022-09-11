1811c1f7cd920bbde8982ebbe7b1a68283e5d5ed
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-09-11 14:05:43 +0100

    updated map visual

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+`
+
+const Footer = () => {
+    return (
+        <Container>
+            Created by Wedge web design
+        </Container>
+    )
+}
+
+export default Footer
